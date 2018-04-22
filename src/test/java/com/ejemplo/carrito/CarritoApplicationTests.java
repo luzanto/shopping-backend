7073c114e27b9f27120940f1fe5f26235775d172
Unknown <pablo.caceres.l@usach.cl>
2018-04-22 01:20:57 -0300

    Auto stash before merge of "master" and "origin/master"
    Add Integration test case

--- src/test/java/com/ejemplo/carrito/CarritoApplicationTests.java
+++ src/test/java/com/ejemplo/carrito/CarritoApplicationTests.java
@@ -16,22 +16,4 @@ public class CarritoApplicationTests {
 	public void contextLoads() {
 	}
 
-	@Test
-	public void defaultTest() {
-		// Se genera un producto p arbitrario
-		Producto p = new Producto();
-		p.setNombre("Teclado");
-		p.setPrecio(10000);
-		p.setCategoria("Importado");
-		p.setVencimiento(null);
-		// Se llama a método dsctoEfectivo con flag efectivo = true
-		int precioDscto = ControladorProducto.dsctoEfectivo(p, true);
-		// Se calcula de manera aparte el posible descuento
-		int precioOriginal = p.getPrecio();
-		Double precioFinal = precioOriginal - precioOriginal*0.19;
-		int precioEsperado = precioFinal.intValue();
-		System.out.println("Precio Original: "+ precioOriginal + "\nPrecio Descuento: " + precioDscto);
-		// Se compara resultado esperado con el obtenido por el método llamado
-		Assert.assertEquals(precioEsperado, precioDscto);
-	}
 }
